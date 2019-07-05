7d9fe138adb94b6a000ae010468530a3a0ca2074
tony <xcc2533919@163.com>
2019-07-05 10:12:22 +0800

    更新头条循环机制

--- tt.py
+++ tt.py
@@ -2,18 +2,18 @@
 from requests import Session
 from time import time
 from hashlib import md5
+import json
 
 s = Session()
 
-def toutiao():
+def toutiao(referer='http://m.toutiao.com/profile/50502347096/', max_behot_time=''):
     pc = 'https://www.toutiao.com/c/user/50502347096/#mid=50502347096'
-    h5 = 'http://m.toutiao.com/profile/50502347096/#mid=50502347096'
     # pc版要解密_signature参数, 这个有一定复杂度; h5版只需解密as和cp参数
     pc_api = 'https://www.toutiao.com/c/user/article/?page_type=1&user_id=50502347096&max_behot_time=0&count=20&as=&cp=&_signature='
     as_, cp_ = tt_encrypt()
-    h5_data = {
+    data = {
         'page_type': '1',
-        'max_behot_time': '',
+        'max_behot_time': max_behot_time,
         'uid': '50502347096',
         'media_id': '50502347096',
         'output': 'json',
@@ -26,13 +26,15 @@ def toutiao():
         'callback': 'jsonp4'
     }
     headers = {
-        'Referer': 'http://m.toutiao.com/profile/50502347096/',
-        'User-Agent': 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)'
+        'Referer': referer,
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36'
     }
     h5_api = 'https://www.toutiao.com/pgc/ma/'
     r = s.get(h5_api, headers=headers, params=h5_data)
-    print(r.url)
-    print(r.text)
+    j = json.loads(r.text)
+    print(j['data'])
+    if j['has_more'] == 1:
+        toutiao(referer, j['next']['max_behot_time'])
 
 def tt_encrypt():
     now = int(time())
